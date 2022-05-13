0eb8a7edfc2b4d4e6b63db86e72851602c91af03
Owen Roth <69156111+oroth8@users.noreply.github.com>
2022-05-12 21:05:36 -0500

    added starting langind page controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'landing_page#index'
+
+  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
