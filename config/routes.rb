38a070ab00d6082b16bc52ce8a620a63586fc3b0
Owen Roth <oroth8@comcast.net>
2022-07-22 14:58:03 -1000

    fixed routes and _alert partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :units
-  resources :organisations
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
-    post "/users/sign_up", to: "users/registrations#create"
-    
+    post '/auth/register/signup', to: "users/registrations#create"
   end
   resources :users, only: %i[index update]
-
+  resources :units
+  resources :organisations
   root "landing_page#index"
   get "/dashboard", to: "dashboard#index", as: "dashboard"
 end
