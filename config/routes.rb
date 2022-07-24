4bc69c4fdb5c15fa64df356da365176c68d57bea
Owen Roth <oroth8@comcast.net>
2022-07-24 15:59:47 -0500

    added missing recourses and css compressor

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :admin do
-      resources :units
-      resources :organisations
-      resources :users
-
-      root to: "units#index"
-    end
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
     post '/auth/register/signup', to: "users/registrations#create"
   end
+  namespace :admin do
+    resources :units
+    resources :organisations
+    resources :users
+
+    root to: "units#index"
+  end
   resources :users, only: %i[index update]
+  resources :organisations
+  resources :units
   root "landing_page#index"
   get "/dashboard", to: "dashboard#index", as: "dashboard"
 end
